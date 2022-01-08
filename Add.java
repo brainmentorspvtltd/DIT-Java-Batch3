f0b68e2c7b85e705464750cb1e9f089147ff8e45
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-01-08 12:57:35 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+
+public strictfp class Add {
+    public static void main(String[] args) {
+        boolean att = true;
+        char w1 = 'न';
+        System.out.println(w1);
+        float w = 90.20f;
+        double d = 90.3434;
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("Sum is " + c);
+    }
+}
---
+++ First.java
@@ -0,0 +1,12 @@
+class Second {
+
+    static void main() {
+        System.out.println("Hi this is my main fn");
+    }
+
+    static public void main(String... arr) {
+        // Execution Start Here
+        System.out.println("Hello Java");
+        // main();
+    }
+}
