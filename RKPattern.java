56f662b2313276d87ba5bb2a694c4fb8c4ccf202
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-06-09 10:31:16 +0530

    Create RKPattern.java

---
+++ RKPattern.java
@@ -0,0 +1,53 @@
+public class RKPattern {
+    public static void main(String[] args) {
+        /*
+         * 1. Slide the Pattern One By One
+         * 2. Do the Better Hash
+         * 3. If Hash are same then compare the characters
+         * 4. Repeat the 1st Step Again till string exhaust
+         */
+        String str = "AXAYABCDAXABZZAWABAD";
+        String pattern = "AB";
+        int n = str.length();
+        int p = pattern.length();
+        int d = pattern.length();
+        int q = 13; // Prime No / Large Prime No (If Hash is coming Negative)
+        int stringHash = 0;
+        int patternHash = 0;
+        // Compute the Pattern Hash First
+        for (int i = 0; i < p; i++) {
+            patternHash = (patternHash * d + (pattern.charAt(i))) % q;
+            stringHash = (stringHash * d + (str.charAt(i))) % q;
+
+        }
+        for (int i = 0; i <= n - p; i++) {
+            if (patternHash == stringHash) {
+                // compare the pattern with string values one by one
+                int j;
+                for (j = 0; j < p; j++) {
+                    if (pattern.charAt(j) != str.charAt(i + j)) {
+                        break;
+                    }
+                } // j loop ends (Pattern loop ends)
+                if (j == p) {
+                    System.out.println("Pattern Match " + i);
+                }
+            }
+                // recompute str Hash only
+                if (i < n - p) {
+
+                    stringHash = stringHash - (str.charAt(i) *d); // Remove the Old char Hash of Slide
+                    stringHash = ((stringHash * d + str.charAt(i + p))) % q; // add the New Char hash of Slide
+
+
+                    if (stringHash < 0) {
+                        stringHash = stringHash + q;
+                    }
+                }
+
+            }
+
+
+    }
+
+}
